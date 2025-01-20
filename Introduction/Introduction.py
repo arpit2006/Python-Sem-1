64ca82220267802b3bb1d891d8655f99e7200038
arpit2006 <arpitshirbhate4@gmail.com>
2025-01-20 18:14:06 +0530

    initial commit

---
+++ Introduction/Introduction.py
@@ -0,0 +1,23 @@
+# Introduction
+# Day 1
+
+print(5 + 2)   # addition(+)
+print(5 - 2)   # subtraction(-)
+print(5 * 2)   # multiplication(*)
+print(5 / 2)   # division(/)
+print(5 ** 2)  # exponential(**)
+print(5 % 2)   # modulus(%)
+print(5 // 2)  # Floor division operator(//)
+
+# Checking data types
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 4j))              # Complex
+print(type('XYX'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name':'XYX'})) # Dictionary
+print(type({9, 3.1, 2}))    # Set
+print(type((9, 3, 2)))    # Tuple
+print(type(3 == 3))              # Bool
+print(type(3 >= 3))              # Bool
